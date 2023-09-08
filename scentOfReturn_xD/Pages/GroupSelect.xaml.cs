7cf81397d21094fe4fe0ac288642679bcb0a60e0
87 <87@DESKTOP-HB85ISA>
2023-09-08 20:46:51 +0300

    shell ready

--- scentOfReturn_xD/Pages/GroupSelect.xaml.cs
+++ scentOfReturn_xD/Pages/GroupSelect.xaml.cs
@@ -4,11 +4,16 @@ public partial class GroupSelect : ContentPage
 {
 	public GroupSelect()
 	{
-		InitializeComponent();
-	}
-
-   async private void ReturnBack(object sender, TappedEventArgs e)
+        InitializeComponent();
+    }
+   
+    async private void ReturnBack(object sender, TappedEventArgs e)
     {
-		await Navigation.PopAsync();
+        await Navigation.PushAsync(new MainPage());        
+    }
+
+    private void GroupPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {  
+            Navigation.PushAsync(new MainPage());
     }
 }
